147b68f168f37fddf86d6c3a25bb78bd8cc801ab
ManinderSinghBakshi <maninder.bakshi@ongrid.in>
2022-09-18 13:31:53 +0530

    first commit

---
+++ src/main/java/com/coders/geeky/GeekyCodersApplication.java
@@ -0,0 +1,13 @@
+package com.coders.geeky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekyCodersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekyCodersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coders/geeky/GeekyCodersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coders.geeky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekyCodersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
